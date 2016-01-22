eca4e0651cf27c5ac60f683c9b479150fd1505a4
Alex Florenti <xela92@MBP-di-Axel.lan>
2016-01-22 13:40:02 +0100

    test

--- src/jmupenwinupdater/JMupenWinupdater.java
+++ src/jmupenwinupdater/JMupenWinupdater.java
@@ -9,6 +9,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
 
@@ -20,7 +22,7 @@ public class JMupenWinupdater {
 
     private static final File updatePackage = new File(getConfigDir().concat(File.separator).concat("jmupen-update.jar"));
     private static File currentJar;
-    private static final File tmpDir = new File(System.getProperty("java.io.tmpdir") + "\\jmupen");
+    private static final File tmpDir = new File(System.getProperty("java.io.tmpdir") + File.separator + "jmupen");
     private static final File currentJarTxtFile = new File(tmpDir.getAbsolutePath() + File.separator + "bin" + File.separator + "currentjar.txt");
 
     /**
@@ -30,10 +32,17 @@ public class JMupenWinupdater {
         final String javaBin = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java.exe";
 
         currentJar = JMupenWinupdater.getCurrentJarFromTxt();
+        JMupenWinupdater.showInfo("currentJarTxtFile:", currentJarTxtFile.getAbsolutePath());
+        JMupenWinupdater.showInfo("currentJar:", currentJar.getAbsolutePath());
+        JMupenWinupdater.showInfo("temporary dir where I take txt file: ", tmpDir.getAbsolutePath());
+        JMupenWinupdater.showInfo(":", currentJar.getAbsolutePath());
+
         try {
             FileUtils.moveFile(updatePackage, currentJar);
         } catch (IOException ex) {
             System.err.println("Error moving update file. " + ex.getLocalizedMessage());
+            JMupenWinupdater.showError("Error moving update file.", ex.getLocalizedMessage());
+
         }
 
         /* Build command: java -jar application.jar */
@@ -47,6 +56,8 @@ public class JMupenWinupdater {
             builder.start();
         } catch (IOException ex) {
             System.err.println("Error restarting app. " + ex.getLocalizedMessage());
+            JMupenWinupdater.showError("Error restarting app. ", ex.getLocalizedMessage());
+
         }
     }
 
@@ -58,6 +69,8 @@ public class JMupenWinupdater {
             path = IOUtils.toString(f);
         } catch (Exception ex) {
             System.err.println("File not found or can't read file " + ex.getLocalizedMessage());
+            JMupenWinupdater.showError("File not found or can't read file", ex.getLocalizedMessage());
+
         }
         if (path == null) {
             path = "";
@@ -67,6 +80,15 @@ public class JMupenWinupdater {
 
     public static String getConfigDir() {
         return System.getProperty("java.io.tmpdir");
+    }
+
+    public static void showError(String mainMsg, String fullMess) {
+        JOptionPane.showMessageDialog(new JFrame(), mainMsg + "\n Full message: " + fullMess, "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    public static void showInfo(String mainMsg, String fullMess) {
+        JOptionPane.showMessageDialog(new JFrame(), mainMsg + "\n Full message: " + fullMess, "Info", JOptionPane.INFORMATION_MESSAGE);
+
     }
 
 }
